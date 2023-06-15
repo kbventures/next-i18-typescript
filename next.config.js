419b7c50ddd240bff7f6e39152bf06a857900853
kbventures <kmb_x@hotmail.com>
2023-06-15 12:03:48 -0500

    Fixed import error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-//next.config.js
+const { i18n } = require("./next-i18next.config");
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  I18n,
-  reactStrictMode: true,
-  swcMinify: true,
-  }
-  module.exports = nextConfig;
+module.exports = {
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "ja"],
+  },
+};
